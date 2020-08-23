0d58eb0424107c20c16a6da6a3c198755f5dff07
Akshay <avj3395@gmail.com>
2020-08-23 14:50:24 +0530

    Updates

---
+++ precache-manifest.e4aea3174a46aa28c013ddd306ade880.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "217dde774139413059c8cac55b27d415",
+    "url": "/avj3395-react-web-chat/index.html"
+  },
+  {
+    "revision": "86721db7c40eabf3bd90",
+    "url": "/avj3395-react-web-chat/static/css/main.a97450fb.chunk.css"
+  },
+  {
+    "revision": "2bfc1e22b0940a26481f",
+    "url": "/avj3395-react-web-chat/static/js/2.439f5caa.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/avj3395-react-web-chat/static/js/2.439f5caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86721db7c40eabf3bd90",
+    "url": "/avj3395-react-web-chat/static/js/main.c95925b1.chunk.js"
+  },
+  {
+    "revision": "9f0cc29dae91cf45a9a1",
+    "url": "/avj3395-react-web-chat/static/js/runtime-main.a480efb6.js"
+  }
+]);
